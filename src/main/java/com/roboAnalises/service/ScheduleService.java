1716e14eb5c6c7fe61682df977db7302eea5123b
MatheusmDiniz <matheus_mendesdiniz@hotmail.com>
2023-03-15 21:54:52 -0300

    fads

--- src/main/java/com/roboAnalises/service/ScheduleService.java
+++ src/main/java/com/roboAnalises/service/ScheduleService.java
@@ -44,18 +44,18 @@ public class ScheduleService {
     @Autowired
     private final EntradasService entradasService;
 
-//    @Scheduled(fixedDelay = 900000)
-//    public void verificarPadroes(){
-//        System.out.println("Rodando Verificar Padroes");
-//        verificarPadroesService.verificarPadroes();
-//
-//    }
-//
-//    @Scheduled(fixedDelay = 60000)
-//    public void principal(){
-//        System.out.println("Rodando O robo");
-//        roboService.iniciarRobo();
-//    }
+    @Scheduled(fixedDelay = 900000)
+    public void verificarPadroes(){
+        System.out.println("Rodando Verificar Padroes");
+        verificarPadroesService.verificarPadroes();
+
+    }
+
+    @Scheduled(fixedDelay = 60000)
+    public void principal(){
+        System.out.println("Rodando O robo");
+        roboService.iniciarRobo();
+    }
 
     @Scheduled(fixedDelay = 300000)
     public void verificarEntradasFinalizadas(){
